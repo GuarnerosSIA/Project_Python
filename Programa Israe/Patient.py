23d43023bc2c35fead589397fa6ee3d1f2e7f9df
GuarnerosSIA <alejandrowarnsan@gmail.com>
2021-03-15 13:55:00 -0600

    Final Programm V1 FR/EN

--- Programa Israe/Patient.py
+++ Programa Israe/Patient.py
@@ -13,17 +13,26 @@ class Patient(Occupant):
   # CONSTRUCTOR
   def __init__(self, _nom, _prenom, _age, _symptomes):
     super().__init__(_nom, _prenom, _age)
-    self.symptomes = eval(_symptomes) # Dictionary creation
-    symptomeLst = Symptome(self.symptomes)
+    tmpDictionary = eval(_symptomes)
+    self.symptomeLst = []
+    some = 0
+    for key, value in tmpDictionary.items():
+        _var = Symptome(key, value)
+        some += value
+        self.symptomeLst.append(_var.__dict__)
     self.type = 'patient' 
-    self.etat = symptomeLst.etat()
+    self.etat = Symptome.severity(some,len(tmpDictionary.items()))
+    self.sick = self.is_sick()
     print(self)
 
   def __repr__(self):
     return '{self.__class__.__name__}({self.nom}, {self.prenom}, {self.age}, {self.etat})'.format(self=self)
 
   def __str__(self):
-    return '>> Le patient {0} {1} {2} (sévérité : {3}) à été ajouté ! (Patient {0} {1} {2} (severity: {3}) has been added!)'.format(self.nom.upper(), self.prenom.capitalize(), self.age, self.etat)
+    myStr = '>> Le patient {0} {1} {2} (sévérité : {3}) à été ajouté ! (Patient {0} {1} {2} (severity: {3}) has been added!) '.format(self.nom.upper(), self.prenom.capitalize(), self.age, self.etat)
+    if self.sick:
+      myStr += "\n The patient is sick"
+    return myStr
 
   # --- IS SICK ---
   def is_sick(self):
--- Programa Israe/Symptome.py
+++ Programa Israe/Symptome.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python3.6
 # -*- coding: utf-8 -*-
-class Symptome:
+
+#This part will be a test for reviewing the posibility of inherity from a dictionary
+
+import json
+
+
+class Symptome(dict):
     """
     CLASS SYMPTOME
     This class encapsulate the possible symptomes asscoiated to a patient
@@ -9,5 +15,6 @@ class Symptome:
     def __init__(self, _nom, _niveau):
         self.nom = _nom
         self.niveau = _niveau
-
-    
+    @staticmethod
+    def severity(some,length):
+        return some/length
